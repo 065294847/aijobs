f8cc038c21c8b00f2c1d5007f76ada8186571d75
065294847 <81414263+065294847@users.noreply.github.com>
2022-05-20 07:45:23 +0200

    Update gpt_3_dash_playground.py

--- gpt_3_dash_playground.py
+++ gpt_3_dash_playground.py
@@ -6,9 +6,9 @@ openai.api_key = st.secrets["SECRET_KEY"]
 
 st.title('GPT-3')
 
-st.text('This GPT-3 model is the Davinci-engine')
-prompt_text = st.text_input(label="Add here phrase, which you want to be completed", value="Add here few words")
-response = openai.Completion.create(engine="curie:ft-qubit-2022-05-18-13-20-56", prompt=prompt_text, max_tokens=50)
+st.text('Denne model kan skrive jobannoncer på kommando.')
+prompt_text = st.text_input(label="Skriv din prompt her, af formen Stilling, Arbejdsplads, kort beskrivelse", value="Add here few words")
+response = openai.Completion.create(engine="curie:ft-qubit-2022-05-18-13-20-56", prompt=prompt_text, max_tokens=200)
 st.text('Remaining phrase:')
 st.text(response["choices"][0]["text"])
 
