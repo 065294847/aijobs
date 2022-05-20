025277fd4de01b7ab95559e7f0c4a4dd9732ca68
065294847 <81414263+065294847@users.noreply.github.com>
2022-05-20 09:18:22 +0200

    Update gpt_3_dash_playground.py

--- gpt_3_dash_playground.py
+++ gpt_3_dash_playground.py
@@ -8,7 +8,11 @@ st.title('GPT-3')
 
 st.text('Denne model kan skrive jobannoncer på kommando.')
 prompt_text = st.text_input(label="Skriv din prompt her, af formen Stilling, Arbejdsplads, kort beskrivelse", value="Input")
-response = openai.Completion.create(model="curie:ft-qubit-2022-05-18-13-20-56", prompt=prompt_text, max_tokens=200)
+response = openai.Completion.create(model="curie:ft-qubit-2022-05-18-13-20-56", prompt=prompt_text, temperature=0.7,
+max_tokens=300,
+top_p=1,
+frequency_penalty=0,
+presence_penalty=0)
 #st.text('Remaining phrase:')
 st.markdown(response["choices"][0]["text"])
 
