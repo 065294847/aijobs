7f03789d7fff9e42782cc869b4f9565ca5763b0e
065294847 <81414263+065294847@users.noreply.github.com>
2022-05-23 10:29:27 +0200

    Update gpt_3_dash_playground.py

--- gpt_3_dash_playground.py
+++ gpt_3_dash_playground.py
@@ -12,7 +12,7 @@ prompt_text1 = st.text_input(label="Arbejdsplads", value="Input")
 prompt_text2 = st.text_input(label="Hvad er den vigtigste arbejdsopgave og kompetence?", value="Input")
 delimiter0=st.text_input(', ')
 delimiter1=st.text_input('. ')
-response = openai.Completion.create(model="curie:ft-qubit-2022-05-18-13-20-56", prompt=prompt_text0+delimiter0+prompt_text1+delimiter1=st.text_input('. ')+prompt_text2, temperature=0.7,
+response = openai.Completion.create(model="curie:ft-qubit-2022-05-18-13-20-56", prompt=prompt_text0+delimiter0+prompt_text1+delimiter1+prompt_text2, temperature=0.7,
 max_tokens=300,
 top_p=1,
 frequency_penalty=0,
