5feed986cf39b835960d5b5c6bcdd9911cc7928b
065294847 <81414263+065294847@users.noreply.github.com>
2022-05-23 10:30:43 +0200

    Update gpt_3_dash_playground.py

--- gpt_3_dash_playground.py
+++ gpt_3_dash_playground.py
@@ -10,8 +10,8 @@ st.text('Denne model kan skrive jobannoncer på kommando.')
 prompt_text0 = st.text_input(label="Stillingsbetegnelse", value="Input")
 prompt_text1 = st.text_input(label="Arbejdsplads", value="Input")
 prompt_text2 = st.text_input(label="Hvad er den vigtigste arbejdsopgave og kompetence?", value="Input")
-delimiter0=st.text_input(', ')
-delimiter1=st.text_input('. ')
+delimiter0=st.text(', ')
+delimiter1=st.text('. ')
 response = openai.Completion.create(model="curie:ft-qubit-2022-05-18-13-20-56", prompt=prompt_text0+delimiter0+prompt_text1+delimiter1+prompt_text2, temperature=0.7,
 max_tokens=300,
 top_p=1,
