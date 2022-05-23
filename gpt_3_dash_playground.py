3fbb984cb12e16dd7e176dab0191a69ac94c1939
065294847 <81414263+065294847@users.noreply.github.com>
2022-05-23 10:58:39 +0200

    Update gpt_3_dash_playground.py

--- gpt_3_dash_playground.py
+++ gpt_3_dash_playground.py
@@ -11,7 +11,7 @@ prompt_text0 = st.text_input(label="Hvad hedder stillingen? Eksempel: Redaktør 
 prompt_text1 = st.text_input(label="Hvilket firma ansætter? Eksempel: DTU", value="Input")
 prompt_text2 = st.text_input(label="Hvad er de vigtigste arbejdsopgaver og kompetencer, som stillingen kræver? Skriv gerne hele sætninger her, og gerne flere sætninger, så performer modellen bedre. Eksempel: DTU er i gang med at styrke sin organisatoriske kommunikation, og vi søger derfor en redaktør til at udvikle et nyhedsbrev til DTU’s interne målgrupper", value="Input")
 response = openai.Completion.create(model="curie:ft-qubit-2022-05-18-13-20-56", prompt=prompt_text0+", "+prompt_text1+". "+prompt_text2, temperature=0.7,
-max_tokens=600,
+max_tokens=800,
 top_p=1,
 frequency_penalty=0,
 presence_penalty=0)
