45ea6112222ef053dad278c2e512aa37e2f99b74
065294847 <81414263+065294847@users.noreply.github.com>
2022-05-23 10:52:37 +0200

    Update gpt_3_dash_playground.py

--- gpt_3_dash_playground.py
+++ gpt_3_dash_playground.py
@@ -7,9 +7,9 @@ openai.api_key = st.secrets["SECRET_KEY"]
 st.title('GPT-3')
 
 st.text('Denne model kan skrive jobannoncer på kommando.')
-prompt_text0 = st.text_input(label="Stillingsbetegnelse", value="Input")
-prompt_text1 = st.text_input(label="Arbejdsplads", value="Input")
-prompt_text2 = st.text_input(label="Hvad er de vigtigste arbejdsopgaver og kompetence? Skriv gerne hele sætninger her, og gerne flere sætninger, så performer modellen bedre", value="Input")
+prompt_text0 = st.text_input(label="Hvad hedder stillingen? Eksempel: Redaktør til internt nyhedsbrev", value="Input")
+prompt_text1 = st.text_input(label="Hvilket firma ansætter? Eksempel: DTU", value="Input")
+prompt_text2 = st.text_input(label="Hvad er de vigtigste arbejdsopgaver og kompetencer, som stillingen kræver? Skriv gerne hele sætninger her, og gerne flere sætninger, så performer modellen bedre. Eksempel: DTU er i gang med at styrke sin organisatoriske kommunikation, og vi søger derfor en redaktør til at udvikle et nyhedsbrev til DTU’s interne målgrupper", value="Input")
 response = openai.Completion.create(model="curie:ft-qubit-2022-05-18-13-20-56", prompt=prompt_text0+", "+prompt_text1+". "+prompt_text2, temperature=0.7,
 max_tokens=400,
 top_p=1,
