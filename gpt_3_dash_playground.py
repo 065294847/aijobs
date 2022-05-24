c3d5cfafd726cae92803d1e95772cf53ee5de52c
065294847 <81414263+065294847@users.noreply.github.com>
2022-05-24 11:11:31 +0200

    Update gpt_3_dash_playground.py

--- gpt_3_dash_playground.py
+++ gpt_3_dash_playground.py
@@ -7,9 +7,9 @@ openai.api_key = st.secrets["SECRET_KEY"]
 st.title('Her skriver AI-robotten dit jobopslag')
 
 st.text('Denne model kan skrive jobannoncer på kommando.')
-prompt_text0 = st.text_input(label="Hvad hedder stillingen? Eksempel: Redaktør til internt nyhedsbrev", value="Input")
-prompt_text1 = st.text_input(label="Hvilket firma ansætter? Eksempel: DTU", value="Input")
-prompt_text2 = st.text_input(label="Hvad er de vigtigste arbejdsopgaver og kompetencer, som stillingen kræver? Skriv gerne hele sætninger her, og gerne flere sætninger, så performer modellen bedre. Eksempel: DTU er i gang med at styrke sin organisatoriske kommunikation, og vi søger derfor en redaktør til at udvikle et nyhedsbrev til DTU’s interne målgrupper", value="Input")
+prompt_text0 = st.text_input(label="Hvad hedder stillingen? Eksempel: Redaktør til internt nyhedsbrev", value="Skriv stillingsbetegnelse her")
+prompt_text1 = st.text_input(label="Hvilket firma ansætter? Eksempel: DTU", value="Skriv navnet på firmaet her")
+prompt_text2 = st.text_input(label="Hvad er de vigtigste arbejdsopgaver og kompetencer, som stillingen kræver? Skriv gerne hele sætninger her, og gerne flere sætninger, så performer modellen bedre. Eksempel: DTU er i gang med at styrke sin organisatoriske kommunikation, og vi søger derfor en redaktør til at udvikle et nyhedsbrev til DTU’s interne målgrupper", value="Skriv et par sætninger om arbejdsopgaver og ansøgers forventede kompetencer her")
 response = openai.Completion.create(model="curie:ft-qubit-2022-05-18-13-20-56", prompt=prompt_text0+", "+prompt_text1+". "+prompt_text2, temperature=0.7,
 max_tokens=800,
 top_p=1,
